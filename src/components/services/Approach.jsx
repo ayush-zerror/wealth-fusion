1cad6185b93f8b2f36233dac06e84f7461e0748e
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-03-24 18:40:24 +0530

    update

--- src/components/services/Approach.jsx
+++ src/components/services/Approach.jsx
@@ -34,7 +34,7 @@ const Approach = () => {
   return (
     <div id='about-section4' className='approach-section'>
       <div className="pt2">
-        <h2>OurApproach</h2>
+        <h2>Our Approach</h2>
         <p>At WealthFusion, our approach to wealth management is deeply rooted in cultivating long-term partnerships and making it more personal. We believe that once we truly understand your unique circumstances and aspirations, we can create a bespoke solution that aligns with your goals and aspirations.</p>
         <p>In a landscape where many firms are shifting toward standardizing their products and services, we have purposefully looked away to maintain our independence and offer tailored advice that fits your personal values and priorities. We recognize that no financial decision is easy, and every choice can significantly influence the financial outcomes for you and your family.</p>
         <p>Be it expanding your business, planning for your family’s future, or leaving a lasting legacy, our role is to be on your side, helping you navigate these difficult decisions with confidence and ensuring that your financial plan evolves with changing circumstances. Whether you’re considering moving abroad, purchasing a new home, or planning for your children’s education, we stand by your side, continuously refining your plan to meet your needs.</p>
